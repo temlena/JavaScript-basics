7acaeb50070af2423b01c64b07fb4285d1a9d4a9
Helen Oridupa <94105502+temlena@users.noreply.github.com>
2023-12-05 05:24:29 +0100

    Create Assignment.js

---
+++ Assignment.js
@@ -0,0 +1,21 @@
+// Assigment. Create a function that checks if the input/parameter is an even number. 
+function checkEven(num){
+  if (num % 2 === 0){
+  console.log('Even number!')
+  }
+  else('Not an even number')
+}
+//Create a function that prints to the console n1 to n2. where n1 is the first input/parameter and n2 is the second input/parameter
+function printNum(n1,n2){
+for(let i = n1, i <= n2, i++){
+  console.log(i)
+}
+}
+//Create a function that sum the values in-between two numbers. eg calcFunc(1,3) returns 6. ie. 1+2+3.
+function calcFunc(n1, n2){
+  let sum = 0
+  for(let i = n1, i <= n2, i++){
+    sum += i
+}
+      return sum
+}
